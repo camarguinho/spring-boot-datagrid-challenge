56251faed6a0aa9d3fab5f0ee012574bc41f554f
user2 <joao.menezes@santander.com.br>
2021-10-01 03:30:45 +0000

    try using local data grid configuration

--- src/main/java/com/redhat/developergames/model/Weather.java
+++ src/main/java/com/redhat/developergames/model/Weather.java
@@ -4,58 +4,61 @@ import java.io.Serializable;
 import java.util.Objects;
 
 public class Weather implements Serializable {
-   private Float temperature;
-   private WeatherCondition condition;
-   private String city;
+    
+    private static final long serialVersionUID = 1L;
 
-   public Weather(Float temperature, WeatherCondition condition, String city) {
+    private Float temperature;
+    private WeatherCondition condition;
+    private String city;
+
+    public Weather(Float temperature, WeatherCondition condition, String city) {
       this.temperature = temperature;
       this.condition = condition;
       this.city = city;
-   }
-
-   public Float getTemperature() {
-      return temperature;
-   }
-
-   public void setTemperature(Float temperature) {
-      this.temperature = temperature;
-   }
-
-   public WeatherCondition getCondition() {
-      return condition;
-   }
-
-   public void setCondition(WeatherCondition condition) {
-      this.condition = condition;
-   }
-
-   public String getCity() {
-      return city;
-   }
-
-   public void setCity(String city) {
-      this.city = city;
-   }
-
-   @Override
-   public boolean equals(Object o) {
-      if (this == o)
-         return true;
-      if (o == null || getClass() != o.getClass())
-         return false;
-      Weather weather = (Weather) o;
-      return Float.compare(weather.temperature, temperature) == 0 && condition == weather.condition && Objects
-            .equals(city, weather.city);
-   }
-
-   @Override
-   public int hashCode() {
-      return Objects.hash(temperature, condition, city);
-   }
-
-   @Override
-   public String toString() {
-      return "Weather{" + "temperature=" + temperature + ", condition=" + condition + ", city='" + city + '\'' + '}';
-   }
+    }
+   
+    public Float getTemperature() {
+       return temperature;
+    }
+   
+    public void setTemperature(Float temperature) {
+       this.temperature = temperature;
+    }
+   
+    public WeatherCondition getCondition() {
+       return condition;
+    }
+   
+    public void setCondition(WeatherCondition condition) {
+       this.condition = condition;
+    }
+   
+    public String getCity() {
+       return city;
+    }
+   
+    public void setCity(String city) {
+       this.city = city;
+    }
+   
+    @Override
+    public boolean equals(Object o) {
+       if (this == o)
+          return true;
+       if (o == null || getClass() != o.getClass())
+          return false;
+       Weather weather = (Weather) o;
+       return Float.compare(weather.temperature, temperature) == 0 && condition == weather.condition && Objects
+             .equals(city, weather.city);
+    }
+   
+    @Override
+    public int hashCode() {
+       return Objects.hash(temperature, condition, city);
+    }
+   
+    @Override
+    public String toString() {
+       return "Weather{" + "temperature=" + temperature + ", condition=" + condition + ", city='" + city + '\'' + '}';
+    }
 }
